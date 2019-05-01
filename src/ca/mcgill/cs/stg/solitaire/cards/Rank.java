8ce5380c746afe75b259dd7101ff48117e68a38f
thisisyomans <tanejamm@gmail.com>
2019-05-01 07:42:16 -0700

    file changes

--- src/ca/mcgill/cs/stg/solitaire/cards/Rank.java
+++ src/ca/mcgill/cs/stg/solitaire/cards/Rank.java
@@ -1,10 +1,6 @@
 /*******************************************************************************
  * Solitaire
  *
- * Copyright (C) 2016-2018 by Martin P. Robillard
- *
- * See: https://github.com/prmr/Solitaire
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
